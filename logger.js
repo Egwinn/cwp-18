2aacc44deb0cb4cee20fea03d6c9eae5a9108413
Marina <dubovik.m14@gmail.com>
2017-12-26 23:15:52 +0200

    add task01-1

---
+++ logger.js
@@ -0,0 +1,11 @@
+const moment = require('moment');
+
+class Logger {
+    constructor(prefix = "prefix", defaultLevel = "LOG", dateFormat = "dddd, MMMM Do YYYY, h:mm:ss a") {
+        this.prefix = prefix;
+        this.defaultLevel = defaultLevel;
+        this.dateFormat = dateFormat;
+    }
+}
+
+module.exports = Logger;
