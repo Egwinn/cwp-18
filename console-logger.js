a10ece16e7e4b8a50b96d9d27b972604d54b26b2
Marina <dubovik.m14@gmail.com>
2017-12-26 23:27:38 +0200

    add task02-2

--- console-logger.js
+++ console-logger.js
@@ -5,6 +5,10 @@ class ConsoleLogger extends Logger {
     constructor(prefix = "prefix", defaultLevel = "LOG", dateFormat = "dddd, MMMM Do YYYY, h:mm:ss a") {
         super(prefix, defaultLevel, dateFormat);
     }
+
+    format(message, level = this.defaultLevel) {
+        return `${moment().format(this.dateFormat)} | ${this.prefix} | ${message}\n`;
+    }
 }
 
 module.exports = ConsoleLogger;
